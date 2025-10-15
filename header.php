445c8ad4ae631fdd35cf488299cfbf0567de9735
mylenezijlstra <mylenezijlstra@icloud.com>
2025-10-15 15:47:03 +0200

    header + footer + index

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php bloginfo('name'); ?></title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+<body>
+<header>
+    <h1><?php bloginfo('name'); ?></h1>
+    <p><?php bloginfo('description'); ?></p>
+</header>
+
--- index.php
+++ index.php
@@ -1,11 +1,28 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<html <?php language_attributes(); ?>>
+<?php get_header(); ?>
 <body>
-  
+    <header>
+        <h1><?php bloginfo('name'); ?></h1>
+        <p><?php bloginfo('description'); ?></p>
+    </header>
+
+    <div id="content">
+        <?php
+        if ( have_posts() ) :
+            while ( have_posts() ) : the_post();
+                ?>
+                <h2><?php the_title(); ?></h2>
+                <div><?php the_content(); ?></div>
+                <?php
+            endwhile;
+        else :
+            echo '<p>Geen berichten gevonden.</p>';
+        endif;
+        ?>
+    </div>
+
+   <?php get_footer(); ?>
 </body>
 </html>
+
